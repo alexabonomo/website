f60a4bbdbe1bc1b13e0f0f7af0b615a6f1d76f5b
Alexa Ann Bonomo <alexa.ann.b@gmail.com>
2019-02-28 16:25:18 -0800

    remove shader language

--- index.js
+++ index.js
@@ -25,21 +25,6 @@ var blotter = new Blotter(material, {
     texts: text
 });
 
-material.uniforms.uSineDistortSpread.value = 0.5;
-material.uniforms.uSineDistortSpread.type = 1f;
-material.uniforms.uSineDistortAmplitude.value = 0.01;
-material.uniforms.uSineDistortAmplitude.type = 1f
-material.uniforms.uNoiseDistortVolatility.value = 20;
-material.uniforms.uNoiseDistortVolatility.type = 1f;
-material.uniforms.uNoiseDistortAmplitude.value = 0.01;
-material.uniforms.uNoiseDistortAmplitude.type = 1f;
-material.uniforms.uDistortPosition.value = 0.5;
-material.uniforms.uDistortPosition.type = 2f;
-material.uniforms.uRotation.value = 170;
-material.uniforms.uRotation.type = 1f;
-material.uniforms.uSpeed.value = 0.08
-material.uniforms.uSpeed.type = 1f;
-
 
 
 
