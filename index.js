ba20959652fd3d52d43c98a011f3f9fec875641d
Alexa Ann Bonomo <alexa.ann.b@gmail.com>
2019-02-28 16:29:20 -0800

    change blotter material

--- index.js
+++ index.js
@@ -9,25 +9,3 @@ imgHover.addEventListener('mouseout', function () {
     imgHover.style.opacity = .75;
     imgHover.src = 'media/me.jpg';
 });
-
-
-var text = new Blotter.Text("Alexa Ann Bonomo", {
-    family: "'Libre Baskerville', serif",
-    size: 27,
-    fill: "#171717",
-    paddingLeft: 40,
-    paddingRight: 40
-});
-
-var material = new Blotter.RollingDistortMaterial();
-
-var blotter = new Blotter(material, {
-    texts: text
-});
-
-
-
-
-var elem = document.getElementById("rollingDistort-text");
-var scope = blotter.forText(text);
-scope.appendTo(elem);
