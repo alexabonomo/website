30f593dcc5ea90afb7ccb180f2563d91c508cb05
Alexa Bonomo <alexabonomo@lexs-mbp.lan>
2021-02-21 01:09:27 -0800

    mobile view

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+if (viewportWidth < 640){
+    console.log ('small viewport')
+    document.querySelector('#img1').style.display="block";
+    document.querySelector('#img2').style.display="block";
+    document.querySelector('#img3').style.display="block";
+    document.querySelector('#img4').style.display="block";
+    document.querySelector('#img5').style.display="block";
+    document.querySelector('#imgMe').style.display="block";
+}
+
+
 function show(){
     document.getElementById('sidebar').classList.toggle('active');
 }
